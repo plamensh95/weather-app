0b220c48ad9eabf2fa359d9cabd85d6320ac938e
Plamen Iliev <plamen.iliev@flatrocktech.com>
2019-05-30 09:02:24 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Plamen Iliev on 30.05.19.
+//  Copyright © 2019 Plamen SH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
